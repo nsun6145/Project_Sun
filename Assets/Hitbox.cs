eaf3f3eb9021533975dc53751cd616a771a79e57
njs6145 <njs6145@g.rit.edu>
2016-12-23 17:25:47 -0600

    Second Set commit

---
+++ Assets/Hitbox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+
+    public int startFrame;
+    public int endFrame;
+
+    public int damage;
+    // Use this for initialization
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Enemy")
+        {
+            Debug.Log("Hitbox collided");
+            Destroy(other.gameObject);
+        }
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+  
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    public float speed;
+    private int count;
+    private Animator animator;
+    private SpriteRenderer sp;
+    bool attackFinish = false; //Prevents animation from playing over and over from holding button down
+    int timer = 0;
+
+    private bool facingLeft = false; //This is so the flip checkboxes aren't being checked every frame
+    private bool facingRight = true;
+
+    //hitboxes
+    public GameObject hitbox1;
+     GameObject currentHitbox;
+
+    // Use this for initialization
+    void Start () {
+
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>(); //get reference
+        sp = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1") && attackFinish == false) {
+            animator.SetTrigger("Player_Attack");
+            if (facingLeft == true) {
+                currentHitbox = Instantiate(hitbox1, -transform.position, Quaternion.identity, this.transform);
+            }
+            else
+           currentHitbox = Instantiate(hitbox1, transform.position, Quaternion.identity, this.transform);
+            //after certain amount of time
+            //Destroy(hitbox1);
+            attackFinish = true;
+        }
+
+        if (Input.GetButtonUp("Fire1") && attackFinish == true)
+        {
+            Destroy(currentHitbox);
+            attackFinish = false;
+        }
+
+        if (Input.GetKeyDown("left") && facingLeft == false) {
+            sp.flipX = true;
+            facingRight = false;
+            facingLeft = true;
+        }
+
+        if (Input.GetKeyDown("right") && facingRight == false)
+        {
+            sp.flipX = false;
+            facingLeft = false;
+            facingRight = true;
+        }
+
+        //if (Input.GetButtonUp("left") || Input.GetButtonUp("right"))
+        //{
+        //    StopMovement();
+        //}
+
+
+    }
+
+    void FixedUpdate() {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(movement* speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.gameObject.CompareTag("PickUp")) {
+        
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+
+        }
+
+    }
+
+    void SetCountText()
+    {
+        //Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
+       // countText.text = "Count: " + count.ToString();
+
+        //Check if we've collected all 12 pickups. If we have...
+        //if (count >= 12)
+            //... then set the text property of our winText object to "You win!"
+         //   winText.text = "You win!";
+    }
+
+    void StopMovement() { //character will not do the slidy
+        rb2d.isKinematic = true;
+    }
+
+    void attack(int hitNum)
+    {
+
+        switch (hitNum)
+        {
+            case 1:
+                Instantiate(hitbox1,this.transform);
+                break;
+        }
+    }
+    
+}
